197a0965be41b1acc6d3eb514933063ca9ce97bf
Leon QL7 Chen <chenql7@lenovo.com>
2023-01-10 15:07:05 +0800

    test

--- test-ui/src/App.tsx
+++ test-ui/src/App.tsx
@@ -4,12 +4,19 @@ import {
   BrowserRouter as Router,Link
 } from "react-router-dom";
 import  router  from './router/router-config';
-import RenderRouter from './router/index'
-function App() {
-  const name = 'helloworld'
+import RenderRouter from './router/index';
+import {connect} from 'react-redux'
+function App(props: { todos: any; }) {
+  const name = 'helloworld';
+  
+  const {todos} = props
   return (
     <div className="App">
-        
+      {
+        todos.map((item: { id: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; },index: any )=>{
+          <div >{item.id}</div>
+        })
+      }
       <div>
         <Router>
           <div>
@@ -22,5 +29,8 @@ function App() {
     </div>
   );
 }
+const mapStateProps = (state: { todos: any; },props: any) =>({
+  todos:state.todos,
+})
 
-export default App;
+export default connect(mapStateProps)(App) ;
--- test-ui/src/index.tsx
+++ test-ui/src/index.tsx
@@ -3,10 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+// 初始化数据state
+const initialState = {
+  todos:[
+    {
+      id:1,
+      text:'hello',
+      completed:false
+    },
+    {
+      id:2,
+      text:'Hi',
+      completed:false
+    }
+  ]
+}
+// 数据关联关系
+const rootReducer = ( state: any, action: any)=>{
+  return state
+}
+// 创建可以追踪的store
+const store = createStore(rootReducer,initialState)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App  />
+    <Provider  store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
